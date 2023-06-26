ba2ef302f906e0e6f4594cb4224dee52b3a7910e
Tiffany Tu <tiffanytu2024@gmail.com>
2023-06-26 10:33:11 -0700

    Initial Commit

---
+++ Interactive Demo/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Interactive Demo
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View
+{
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
+    var body: some View
+    {
+        VStack
+        {
+            Text(textTitle)
+                .font(.title)
+            
+            TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            
+            Button("Submit Name") {
+                textTitle = "Welcome, \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        ContentView()
+    }
+}
---
+++ Interactive Demo/Interactive_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_DemoApp.swift
+//  Interactive Demo
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
